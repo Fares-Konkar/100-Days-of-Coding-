18e2787b0168c116e04ca4c9093995943803df04
Fares Konkar <wwwfareswww@hotmail.com>
2019-08-21 21:25:11 +0300

    DAY 2

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-## First Day:
+## Day 1:
 
 print('Hello World!')
 
 if 5>2 :
-    print('Five is grater than two!')s
+    print('Five is grater than two!')
+
+## Day 2:
+    
+# this comment
+"""
+this is also a comment 
+"""
+print('my name is fares')# this is antother way of comminting 
