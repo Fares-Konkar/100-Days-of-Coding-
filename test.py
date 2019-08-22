ad235bcc0a4a31459c677152c3a883b61b1c277c
Fares Konkar <wwwfareswww@hotmail.com>
2019-08-22 18:58:24 +0300

    DAY 3

--- test.py
+++ test.py
@@ -7,8 +7,17 @@ if 5>2 :
 
 ## Day 2:
     
-# this comment
+# this is a comment
 """
 this is also a comment 
 """
 print('my name is fares')# this is another way of comminting 
+
+## Day 3
+
+# assining varibles
+
+x, y = 20, 'Ahmad' # we can assign either constant or string to a varibles
+print('My name is: ' + y + " my age is: " + str(x)) # number cant be printed, it need to get transfered to string 
+# we can't add string to a number:
+"""print('x+y')"""
