1623e6aa168251ad28a1d8f45a3e7bf8ecfc2189
Fares Konkar <wwwfareswww@hotmail.com>
2019-08-25 21:19:07 +0300

    Week 2

---
+++ week 2.py
@@ -0,0 +1,10 @@
+# DAY 6:
+
+# this is a away to change types 
+
+x = int(1.0) # from float 
+y = float('4') # from string 
+z = str(2) # from integer 
+print(x) # integer 
+print(y) # float 
+print(z) # string 
