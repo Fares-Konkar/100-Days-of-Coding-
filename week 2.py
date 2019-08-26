5a640b4644f218936971869a914517df93b6ce2d
Fares Konkar <wwwfareswww@hotmail.com>
2019-08-27 00:28:10 +0300

    DAY 6

--- week 2.py
+++ week 2.py
@@ -8,3 +8,13 @@ z = str(2) # from integer
 print(x) # integer 
 print(y) # float 
 print(z) # string 
+
+# DAY 7:
+
+a = """This is a multiline string and this varibles could have many lines"""
+print(a)
+b = 'hello world'
+# to print a latter from a string 
+print(b[1])
+# or to print as many as we want 
+print(b[4:7])
